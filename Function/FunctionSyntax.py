6fb62faa31b5ffd476e8a8c4bca5acb690e7fbd6
Yash Khati <160757711+Yash-codeAndDev@users.noreply.github.com>
2024-05-02 14:26:18 +0000

    Function Basics

---
+++ Function/FunctionSyntax.py
@@ -0,0 +1,23 @@
+num = 5
+"""
+    def function_name ( parameters ):
+        function body
+
+"""
+
+# ans = square(10)
+def square ( num):
+    return num*num
+
+ans = square(10)
+print(ans)
+
+
+# Function Having multiple parameter
+def product(a, b) :
+    return a*b
+
+print(product(2, 8))
+print(product('a', 8))
+print(product(2, 'a'))
+
